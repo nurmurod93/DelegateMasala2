68ef092e3c0c8c71ed3276c551dfe0d860871427
Humoyun Ochilov Ravshan o'g'li <43071439+dotNet-developer1995@users.noreply.github.com>
2024-02-15 12:14:05 +0500

    Add project files.

---
+++ DelegateMasala2/PhotoProtsessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateMasala2
+{
+    public class PhotoProtsessor
+    {
+        public delegate void PhotoFilter();
+    }
+}
